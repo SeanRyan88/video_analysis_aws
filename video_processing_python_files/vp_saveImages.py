d5767985367deadfe3a30c05e7dbabd6b55f87a4
Sean Ryan <strya2@student.monash.edu>
2024-05-11 14:37:04 +1000

    save

--- video_processing_python_files/vp_saveImages.py
+++ video_processing_python_files/vp_saveImages.py
@@ -21,11 +21,11 @@ import numpy as np
 
 def SaveImage(image, filename, directory="/"):
     print("Run Process SaveImage")
-    # print("Shape of the image:", image.shape)  # Should show (rows, cols, 3) for color images or (rows, cols) for grayscale
-    print("Data type of the image:", image.dtype)  # Typically should be uint8
-    if isinstance(image, np.ndarray):
+    
+    # if isinstance(image, np.ndarray):
+    try:
         save_path = os.path.join(directory, filename)
         cv2.imwrite(save_path, image)
         print(f"Image saved at {save_path}")
-    else:
+    except:
         print("Provided image is not a valid numpy array.")
