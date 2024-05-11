c5a1c5791f7ae7bcef4fc8a1ab4e48f1484d882d
Sean Ryan <strya2@student.monash.edu>
2024-05-11 14:22:52 +1000

    save

--- video_processing_python_files/vp_saveImages.py
+++ video_processing_python_files/vp_saveImages.py
@@ -21,7 +21,7 @@ import numpy as np
 
 def SaveImage(image, filename, directory="/"):
     print("Run Process SaveImage")
-    print("Shape of the image:", image.shape)  # Should show (rows, cols, 3) for color images or (rows, cols) for grayscale
+    # print("Shape of the image:", image.shape)  # Should show (rows, cols, 3) for color images or (rows, cols) for grayscale
     print("Data type of the image:", image.dtype)  # Typically should be uint8
     if isinstance(image, np.ndarray):
         save_path = os.path.join(directory, filename)
